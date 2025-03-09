6f46796c2b02b44bdc0bce6a42c3fcf2ceb7de3d
Jeffrey van Pelt <jeff@vanpelt.one>
2025-03-09 15:56:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Base Ansible Execution Environment
+This Ansible EE contains the following tools:
+
+  * assh (https://github.com/moul/assh)
+  * VMWare Python SDK
+  * ARA client
+  * Various frequently used Python libraries for Ansible community modules
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Build Execution environment
+SOFTWARE="ansible-builder podman"
+
+function sanity_check() {
+   ok=1
+   for prog in $SOFTWARE
+   do
+      which $prog > /dev/null || { echo "Missing program $prog."; ok=0; }
+   done
+   test $ok -ne 1 && { echo "Sanity check failed. Aboring." ; exit 1; }
+}
+
+sanity_check
+
+ANSIBLE_VERSION=$(grep ansible-core execution-environment.yml | cut -d= -f3)
+ansible-builder build -v3 -t ee-base-$ANSIBLE_VERSION.yml:latest
